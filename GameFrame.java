4f50038b9a524ebddc5b070089a351e656467d78
John <John@10.0.0.96>
2018-03-08 19:02:15 -0500

    I created a new class containing a simple framer

---
+++ GameFrame.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class GameFrame extends Frame implements WindowListener{
+
+	
+	
+	public GameFrame () {
+		setLayout(new FlowLayout());		// We may need to change the type of layout. This arranges
+										// components from left to right with in the Frame
+		addWindowListener(this);
+		setTitle("Defend-Earth");
+		setSize(1000, 1500);
+		setVisible(true);
+	}
+	
+	public static void main (String[] args) {
+		new GameFrame();
+	}
+	
+	@Override
+	public void windowClosing(WindowEvent evt) {
+		System.exit(0);
+	}
+		//Below is needed for above to compile
+			@Override public void windowOpened(WindowEvent evt) {}
+			@Override public void windowClosed(WindowEvent evt) {}
+			@Override public void windowIconified(WindowEvent evt) {}
+			@Override public void windowDeiconified(WindowEvent evt) {}
+			@Override public void windowActivated(WindowEvent evt) {}
+			@Override public void windowDeactivated(WindowEvent evt) {}
+			
+}
