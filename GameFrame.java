a05f0104a9a4dab9ff43590f68c973757b5b65eb
tyler <tyler@DESKTOP-I0B3U1K>
2018-03-15 17:12:45 -0400

    Group work from 03/15 menu set up with printlines for check preliminary title work colors and borders are availabe to change

--- GameFrame.java
+++ GameFrame.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 
-import javax.swing.BoxLayout;
+import javax.swing.*;
 
 import java.awt.color.*;
 
@@ -10,24 +10,52 @@ public class GameFrame extends Frame implements WindowListener, ActionListener{
 
 	private Button btnPlay, btnOption, btnTutorial, btnExit;
 	
+	private Label title;
+	
+	private Frame mainframe;
+	
 	public GameFrame () {
-		setLayout(new FlowLayout());		// We may need to change the type of layout. This arranges
-										// components from left to right with in the Frame
-		Panel menubtnDisplay = new Panel(new GridLayout(4,1));
+		setLayout(new FlowLayout()); // We may need to change the type of layout. This arranges
+									// components from left to right with in the Frame
+		
+
+		Panel menubtnDisplay = new Panel(new GridLayout(5,1));
+		
+		title= new Label("Defend Earth",SwingConstants.TOP);
+		title.setForeground(Color.orange);
+		title.setFont(new Font("Sans Serif",Font.BOLD, 70));
+		menubtnDisplay.add(title);
+		
 		btnPlay = new Button("Play");
 		menubtnDisplay.add(btnPlay);
+		btnPlay.setPreferredSize(new Dimension(300,50));
+		btnPlay.setFont(new Font("Sans Serif", Font.BOLD, 28));
+		btnPlay.setForeground(Color.orange);
+		btnPlay.setBackground(Color.black);
 		btnPlay.addActionListener(this);
 		
 		btnOption = new Button("Options");
 		menubtnDisplay.add(btnOption);
+		btnOption.setPreferredSize(new Dimension(300,50));
+		btnOption.setFont(new Font("Sans Serif", Font.BOLD, 28));
+		btnOption.setForeground(Color.orange);
+		btnOption.setBackground(Color.black);
 		btnOption.addActionListener(this);
 		
 		btnTutorial = new Button("Tutorial");
 		menubtnDisplay.add(btnTutorial);
+		btnTutorial.setPreferredSize(new Dimension(300,50));
+		btnTutorial.setFont(new Font("Sans Serif", Font.BOLD, 28));
+		btnTutorial.setForeground(Color.orange);
+		btnTutorial.setBackground(Color.black);
 		btnTutorial.addActionListener(this);
 		
 		btnExit = new Button("Exit");
 		menubtnDisplay.add(btnExit);
+		btnExit.setPreferredSize(new Dimension(300,50));
+		btnExit.setFont(new Font("Sans Serif", Font.BOLD, 28));
+		btnExit.setForeground(Color.orange);
+		btnExit.setBackground(Color.black);
 		btnExit.addActionListener(this);
 		
 		setLayout(new GridBagLayout());
@@ -35,7 +63,7 @@ public class GameFrame extends Frame implements WindowListener, ActionListener{
 		gbc.gridwidth = GridBagConstraints.REMAINDER;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		add(menubtnDisplay, gbc);
-				
+		
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //This is the getting screen size
 		int width = (int) screenSize.getWidth();
 		int height = (int) screenSize.getHeight();
@@ -43,7 +71,8 @@ public class GameFrame extends Frame implements WindowListener, ActionListener{
 		addWindowListener(this);
 
 		setTitle("Defend-Earth");
-		setSize(width, height);
+		setBackground(Color.black);
+		setSize(1000,600);
 		setVisible(true);
 	}
 	
@@ -51,20 +80,20 @@ public class GameFrame extends Frame implements WindowListener, ActionListener{
 		new GameFrame();
 	}
 	
-	
-	public void actionPerformed1(ActionEvent e) {
+	@Override
+	public void actionPerformed(ActionEvent e) {
 	
 		if(e.getSource() == btnPlay) {
-			System.out.print("Play was selected");
+			System.out.println("Play was selected");
 		}
 		if(e.getSource() == btnOption) {
-			System.out.print("Option was selected");
+			System.out.println("Option was selected");
 		}
 		if(e.getSource() == btnTutorial) {
-			System.out.print("Tutorial was selected");
+			System.out.println("Tutorial was selected");
 		}
 		if(e.getSource() == btnExit) {
-			System.out.print("Exit was selected");
+			System.out.println("Exit was selected");
 			System.exit(0);
 		}
 	}
@@ -80,11 +109,5 @@ public class GameFrame extends Frame implements WindowListener, ActionListener{
 			@Override public void windowDeiconified(WindowEvent evt) {}
 			@Override public void windowActivated(WindowEvent evt) {}
 			@Override public void windowDeactivated(WindowEvent evt) {}
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
 			
 }
